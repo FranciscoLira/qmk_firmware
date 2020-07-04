ba61049ddec15b066d423e77518a081e476b572b
FranciscoLira <arjot12@gmail.com>
2020-07-04 19:06:09 +0100

    Added CeSIUM logo, EE_HANDS

--- keyboards/kyria/keymaps/franclira/rules.mk
+++ keyboards/kyria/keymaps/franclira/rules.mk
@@ -1,3 +1,5 @@
 OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
-ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+ENCODER_ENABLE = no      # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
+
+BOOTLOADER = caterina
